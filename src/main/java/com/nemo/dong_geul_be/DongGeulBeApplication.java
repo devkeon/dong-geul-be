e12f1f6e4c120fa232620639b4f79b54cdc1ce97
Keon <lune0410k@naver.com>
2024-10-19 22:17:42 +0900

    initial commit

---
+++ src/main/java/com/nemo/dong_geul_be/DongGeulBeApplication.java
@@ -0,0 +1,14 @@
+package com.nemo.dong_geul_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class DongGeulBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongGeulBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nemo/dong_geul_be/DongGeulBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemo.dong_geul_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongGeulBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
