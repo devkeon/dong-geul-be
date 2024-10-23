bb5a9987d26bf95ac6e2d21e422973bfd610317c
soychan27 <sychan0327@gmail.com>
2024-10-24 04:14:38 +0900

    feat: add CommentRepository & PostRepository

---
+++ src/main/java/com/nemo/dong_geul_be/board/repository/PostRepository.java
@@ -0,0 +1,19 @@
+package com.nemo.dong_geul_be.board.repository;
+
+import com.nemo.dong_geul_be.board.domain.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByPostTypeTrue();    // 재잘재잘
+
+    List<Post> findByPostTypeFalse();   // 동글동글
+
+    List<Post> findByIsExternalFalse(); // 교내
+
+    List<Post> findByIsExternalTrue();  // 교외
+
+    List<Post> findByPostTypeFalseAndIsExternalFalse(); // 동글동글, 교내
+
+    List<Post> findByPostTypeFalseAndIsExternalTrue();  // 동글동글, 교외
+}
