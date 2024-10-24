fd9ae4beb1fe8d39934179cf74987384d2e8882f
LGY010011 <gylm1111pm@gmail.com>
2024-10-24 19:15:41 +0900

    feat: add mypage domain class

---
+++ src/main/java/com/nemo/dong_geul_be/mypage/domain/MyClub.java
@@ -0,0 +1,52 @@
+package com.nemo.dong_geul_be.mypage.domain;
+
+
+import com.nemo.dong_geul_be.clubAndHeadmem.ClubAndHeadMem;
+import com.nemo.dong_geul_be.member.domain.entity.Member;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MyClub {
+
+        @Id
+        @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+        private Long id;
+
+        @NotNull
+        private String name;
+
+        @NotNull
+        private String studentId;
+
+        @Enumerated(EnumType.STRING)
+        @Column(nullable = false)
+        private IsConfirmed isConfirmed;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "clubAndHeadMem_id")
+        private ClubAndHeadMem clubAndHeadMem;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "member_id")
+        private Member member;
+
+        public void confirmMember() {
+                if (this.isConfirmed == IsConfirmed.JOIN) {
+                        throw new IllegalStateException("Member is already JOIN.");
+                }
+                this.isConfirmed = IsConfirmed.JOIN; // IsConfirmed.CONFIRMED로 변경
+        }
+
+        public void cancelMember() {
+                if (this.isConfirmed == IsConfirmed.REJECT) {
+                        throw new IllegalStateException("Member is already CANCEL.");
+                }
+                this.isConfirmed = IsConfirmed.REJECT; // IsConfirmed.CANCEL로 변경
+        }
+}
