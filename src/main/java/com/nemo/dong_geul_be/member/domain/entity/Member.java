ac6a703a4b6d7e5c3fe46ff6496ef35ca48fc782
LGY010011 <gylm1111pm@gmail.com>
2024-10-24 19:11:57 +0900

    fix: add MANAGER role and map Member to MyClub

--- src/main/java/com/nemo/dong_geul_be/member/domain/entity/Member.java
+++ src/main/java/com/nemo/dong_geul_be/member/domain/entity/Member.java
@@ -1,17 +1,9 @@
 package com.nemo.dong_geul_be.member.domain.entity;
 
+import com.nemo.dong_geul_be.mypage.domain.MyClub;
+import jakarta.persistence.*;
 import org.hibernate.annotations.SQLRestriction;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -19,6 +11,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Builder
 @Entity
@@ -52,6 +47,9 @@ public class Member {
 	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "auth_code")
 	private AuthCode authCode;
+
+	@OneToMany(mappedBy = "member")
+	private List<MyClub> myClubs = new ArrayList<>();
 
 	public void updatePassword(String encryptPassword) {
 		this.password = encryptPassword;
