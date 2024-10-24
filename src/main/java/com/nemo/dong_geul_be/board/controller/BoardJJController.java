483c985413c65c5d2aefc55abddaec8d98f23ab8
davinseo <sdabins2@gmail.com>
2024-10-24 16:24:21 +0900

    feat : 재잘재잘 기능 구현

--- src/main/java/com/nemo/dong_geul_be/board/controller/BoardJJController.java
+++ src/main/java/com/nemo/dong_geul_be/board/controller/BoardJJController.java
@@ -1,13 +1,16 @@
 package com.nemo.dong_geul_be.board.controller;
 
 
+import com.nemo.dong_geul_be.board.domain.dto.request.CreatePostRequest;
+import com.nemo.dong_geul_be.board.domain.dto.response.PostDTO;
+import com.nemo.dong_geul_be.board.domain.dto.response.PostDetailResponse;
+import com.nemo.dong_geul_be.board.domain.entity.Comment;
 import com.nemo.dong_geul_be.board.domain.entity.Post;
+import com.nemo.dong_geul_be.board.service.CommentService;
 import com.nemo.dong_geul_be.board.service.PostService;
 import com.nemo.dong_geul_be.global.response.Response;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,12 +20,51 @@ import java.util.List;
 public class BoardJJController {    //재잘재잘 : 자유게시판
 
     private final PostService postService;
+    private final CommentService commentService;
 
     //메인페이지
     @GetMapping("") // 재잘재잘은 booelean이 1인 게시글
-    public Response<List<Post>> getTruePostTypePosts() {
-        List<Post> posts = postService.getTruePostTypePosts();
+    public Response<List<PostDTO>> getTruePostTypePosts() {
+        List<PostDTO> posts = postService.getTruePostTypePosts();
         return Response.ok(posts);
+    }
+
+    // 재잘재잘, 교내
+    @GetMapping("/on")
+    public Response<List<PostDTO>> getPostTrueExternalFalsePosts() {
+        List<PostDTO> posts = postService.getPostTrueExternalFalsePosts();
+        return Response.ok(posts);
+    }
+
+    // 재잘재잘, 교외
+    @GetMapping("/off")
+    public Response<List<PostDTO>> getPostTrueExternalTruePosts() {
+        List<PostDTO> posts = postService.getPostTrueExternalTruePosts();
+        return Response.ok(posts);
+    }
+
+    // 게시글 상세보기
+    @GetMapping("/{postId}")
+    public Response<PostDetailResponse> getPostDetails(@PathVariable Long postId) {
+        Post post = postService.getPostById(postId);
+        List<Comment> comments = commentService.getCommentsByPost(postId);
+
+        PostDetailResponse response = new PostDetailResponse(post, comments);
+        return Response.ok(response);
+    }
+
+    // 글 작성
+    @PostMapping("/create")
+    public Response<Post> createJejalPost(@RequestBody CreatePostRequest createPostRequest) {
+        Post newPost = postService.createJejalPost(createPostRequest);
+        return Response.ok(newPost);
+    }
+
+    // 댓글 작성
+    @PostMapping("/{postId}/comment")
+    public Response<Comment> createComment(@PathVariable Long postId, @RequestBody String content) {
+        Comment newComment = commentService.createComment(postId, content);
+        return Response.ok(newComment);
     }
 
 }
