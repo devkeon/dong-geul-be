45253e27800fc1b3a61d0124bf71715f1a2896e7
GeonHui Lee <spongging@gmail.com>
2024-10-26 23:13:52 +0900

    [fix] 최근 검색기록 유저 id SecurityContextUtil 로 변경

--- src/main/java/com/nemo/dong_geul_be/Search/controller/SearchController.java
+++ src/main/java/com/nemo/dong_geul_be/Search/controller/SearchController.java
@@ -5,6 +5,7 @@ import com.nemo.dong_geul_be.Search.request.SearchLogSaveRequest;
 import com.nemo.dong_geul_be.Search.response.SearchResponse;
 import com.nemo.dong_geul_be.Search.service.SearchLogService;
 import com.nemo.dong_geul_be.Search.service.SearchService;
+import com.nemo.dong_geul_be.authentication.util.SecurityContextUtil;
 import com.nemo.dong_geul_be.board.domain.dto.response.PostDTO;
 import com.nemo.dong_geul_be.board.domain.entity.Post;
 import com.nemo.dong_geul_be.global.response.Response;
@@ -23,12 +24,12 @@ public class SearchController {
 
     private final SearchLogService searchLogService;
     private final SearchService searchService;
+    private final SecurityContextUtil securityContextUtil;
 
     @Operation(summary = "최근 검색 기록 조회", description = "최근 검색 기록을 조회합니다.")
     @GetMapping("/searchLogs")
-    public ResponseEntity<SearchResponse> findRecentSearchLog(@RequestParam(required = false) Long userId) {
-        // 만약 userId가 필요하다면 쿼리 파라미터로 받는 것이 좋습니다.
-        List<SearchLog> recentSearchLogList = searchLogService.findRecentSearchLogs(userId);
+    public ResponseEntity<SearchResponse> findRecentSearchLog() {
+        List<SearchLog> recentSearchLogList = searchLogService.findRecentSearchLogs(securityContextUtil.getContextMemberInfo().getMemberId());
         return ResponseEntity.ok(new SearchResponse(recentSearchLogList, "최근 검색 기록 조회 완료"));
     }
 
@@ -37,7 +38,7 @@ public class SearchController {
     @Operation(summary = "검색 후 게시물 조회", description = "검색 키워드를 바탕으로 제목, 내용 및 해시태그가 일치하는 게시물 조회 후, 검색 기록 저장")
     @GetMapping("/search")
     public Response<List<PostDTO>> searchPosts(@RequestParam String keyword) {
-        searchLogService.saveRecentSearchLog(1L, keyword); // 1L은 임시 userId
+        searchLogService.saveRecentSearchLog(securityContextUtil.getContextMemberInfo().getMemberId(), keyword); // 1L은 임시 userId
 
         List<Post> posts = searchService.searchPosts(keyword);
 
